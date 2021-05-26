b548f4c51e107cc26aa1d8f93dea7de941341b4a
RibbitWarrior <kyeyunejacob8@gmail.com>
2021-05-26 14:31:19 -0700

    added geocoder

--- assests/scripts/dist/map.js
+++ assests/scripts/dist/map.js
@@ -1,10 +1,24 @@
-//var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+var movies = [-118.8852489, 34.182057];
+var park = [-118.88335, 34.1932084];
+var centerMap = movies;
+var mapboxgl;
+var MapboxGeocoder;
 mapboxgl.accessToken = 'pk.eyJ1IjoicmliYml0d2FycmlvciIsImEiOiJja3A1cTJmdzMwOTAxMnVzYmQ4OHlmb3JwIn0.VC5-U832snyQzljaNYdDrQ';
 var map = new mapboxgl.Map({
     container: "map",
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-118.88335, 34.1932084],
-    zoom: 15 // starting zoom
+    center: centerMap,
+    zoom: 17 // starting zoom
 });
-//satilite style: mapbox://styles/mapbox/satellite-v9
-//street style: mapbox://styles/mapbox/streets-v11
+map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+}));
+// to make a marker
+/*
+var marker = new mapboxgl.Marker() // initialize a new marker
+  .setLngLat([]) // Marker [lng, lat] coordinates
+  .addTo(map); // Add the marker to the map
+
+
+*/ 
