c7511fa3c7bfaad6a09109c2a63183997897670f
RibbitWarrior <kyeyunejacob8@gmail.com>
2021-06-08 15:29:18 -0700

    working navbar

---
+++ assests/scripts/nav.ts
@@ -0,0 +1,43 @@
+
+//functions to run automatically
+searchDates()
+
+async function searchDates(){
+
+document.getElementById("nav-searchbar").addEventListener("focusin", function(){
+
+document.getElementById("searchbar-dropdown").style.display = "block"
+
+})
+
+document.getElementById("nav-searchbar").addEventListener("focusout", function(){
+
+document.getElementById("searchbar-dropdown").style.display = "none"
+
+
+})
+
+
+
+
+
+
+
+document.getElementById("nav-searchbar").addEventListener("keyup", () => {
+//@ts-ignore
+var searchquery = document.querySelector("#nav-searchbar").value
+
+
+console.log(searchquery)
+
+
+if(searchquery == ""){
+
+document.getElementById("searchbar-dropdown").style.display = "none"
+
+}
+
+
+})
+
+}
