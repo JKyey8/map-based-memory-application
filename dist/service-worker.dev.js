84ab3c06caadb3f55e26b3abb4a62ee48d066d1f
RibbitWarrior <kyeyunejacob8@gmail.com>
2021-11-07 19:41:24 -0800

    offline support

--- dist/service-worker.dev.js
+++ dist/service-worker.dev.js
@@ -1,10 +1,22 @@
 "use strict";
 
-// install service worker
+var staticCacheName = "site-static";
+var assets = ['/', "/assests/styles/index.css", "/views/pages/index.ejs", "/assests/styles/nav.css", "/views/partials/nav.ejs", "/assests/scripts/dist/index.js", "/manifest.json", "/assests/styles/dist/index.css", "/assests/styles/dist/nav.css", "/icons/apple-icon-180.png", "/icons/apple-splash-1242-2208.jpg", "icons/manifest-icon-192.maskable.png", "icons/manifest-icon-512.maskable.png"]; // install service worker
+
 self.addEventListener("install", function (evt) {
   console.log("service worker has been installed");
+  evt.waitUntil(caches.open(staticCacheName).then(function (cache) {
+    console.log("caching assets");
+    cache.addAll(assets);
+  }));
 }); // activate service woeker
 
 self.addEventListener("activate", function (evt) {
   console.log('service worker has ben activated');
+}); //fetch event
+
+self.addEventListener('fetch', function (evt) {
+  evt.respondWith(caches.match(evt.request).then(function (cahceRes) {
+    return cahceRes || fetch(evt.request);
+  }));
 });
--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,36 @@
+const staticCacheName = "site-static";
+const assets = [
+
+'/',
+"/assests/styles/index.css", 
+
+ "/views/pages/index.ejs", 
+"/assests/styles/nav.css",
+"/views/partials/nav.ejs",
+"/assests/scripts/dist/index.js",
+"/manifest.json",
+"/assests/styles/dist/index.css",
+"/assests/styles/dist/nav.css",
+"/icons/apple-icon-180.png",
+"/icons/apple-splash-1242-2208.jpg",
+"icons/manifest-icon-192.maskable.png",
+"icons/manifest-icon-512.maskable.png",
+
+
+ ];
+
+
 // install service worker
 
 self.addEventListener("install", (evt) => {
 
 console.log("service worker has been installed")
+evt.waitUntil(
+caches.open(staticCacheName).then(cache => {
+console.log("caching assets")
+cache.addAll(assets)
+
+}))
 
 });
 
@@ -11,6 +39,22 @@ console.log("service worker has been installed")
 
 self.addEventListener("activate", (evt) => {
 console.log('service worker has ben activated')
+
+});
+
+
+//fetch event
+self.addEventListener('fetch', (evt) => {
+evt.respondWith(
+caches.match(evt.request).then(cahceRes => {
+
+return cahceRes || fetch(evt.request);
+
+
+})
+
+
+)
 
 });
 
