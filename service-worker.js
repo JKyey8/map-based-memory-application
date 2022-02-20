68a88d0a63197ecb7ee3c2e89066632ce1a8f42f
RibbitWarrior <kyeyunejacob8@gmail.com>
2022-02-20 14:02:30 -0800

    status bar changes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v1.0.10';
-const dynamicCacheName = 'site-dynamic-v1.0.10';
+const staticCacheName = 'site-static-v1.0.2';
+const dynamicCacheName = 'site-dynamic-v1.0.2';
 const assets = [
 
 '/',
